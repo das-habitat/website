6ae08f2fc0be16065b2a9660fd578cb15c283a0b
Florian Pichler <fp@ylk.gd>
2019-05-19 17:49:00 +0200

    Update hugo

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-curl -L -O https://github.com/gohugoio/hugo/releases/download/v0.55.5/hugo_0.55.5_Linux-64bit.tar.gz
-tar -xzf hugo_0.55.5_Linux-64bit.tar.gz
+curl -L -O https://github.com/gohugoio/hugo/releases/download/v0.55.6/hugo_0.55.6_Linux-64bit.tar.gz
+tar -xzf hugo_0.55.6_Linux-64bit.tar.gz
