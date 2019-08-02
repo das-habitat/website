5efd439a686fc8a817e54704e8df5fd36e5a796d
Florian Pichler <fp@ylk.gd>
2019-08-02 19:04:25 +0200

    Update dependencies

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-curl -L -O https://github.com/gohugoio/hugo/releases/download/v0.55.6/hugo_0.55.6_Linux-64bit.tar.gz
-tar -xzf hugo_0.55.6_Linux-64bit.tar.gz
+curl -L -O https://github.com/gohugoio/hugo/releases/download/v0.56.3/hugo_0.56.3_Linux-64bit.tar.gz
+tar -xzf hugo_0.56.3_Linux-64bit.tar.gz
