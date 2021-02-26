798f5f539eb18158d1c0ff5f6714d6f5daadc8f4
Florian Pichler <fp@ylk.gd>
2021-02-26 22:57:12 +0100

    List styling

--- kirby/site/templates/project.php
+++ kirby/site/templates/project.php
@@ -1,26 +1,25 @@
 <?php snippet('header') ?>
 
-<main>
+<main class="t:project-main">
   <h1><?= $page->title() ?></h1>
 
   <?php snippet('partials/blocks', ['blocks' => $page->text()]); ?>
 </main>
 
-<aside>
-  <ul>
-    <?php foreach($page->siblings() as $child): ?>
-      <li>
-        <?php if($child->isActive()): ?>
-          XXX
-        <?php else: ?>
-        <a href="<?= $child->url() ?>">
-          <img src="" />
-          <h2><?= $child->title() ?></h2>
-        </a>
-        <?php endif; ?>
-      </li>
-    <?php endforeach; ?>
-  </ul>
+<aside class="t:project-side">
+  <nav>
+    <ul>
+      <?php foreach($page->siblings() as $child): ?>
+        <li>
+          <?php if($child->isActive()): ?>
+            <strong><?= $child->title() ?></strong>
+          <?php else: ?>
+            <a href="<?= $child->url() ?>"><?= $child->title() ?></a>
+          <?php endif; ?>
+        </li>
+      <?php endforeach; ?>
+    </ul>
+  </nav>
 </aside>
 
 <?php snippet('footer') ?>
