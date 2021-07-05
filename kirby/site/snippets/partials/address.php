52528ab1adf76294550ec6d7812786f2a37d5423
Florian Pichler <fp@ylk.gd>
2021-07-05 20:06:38 +0200

    Add new phone number

--- kirby/site/snippets/partials/address.php
+++ kirby/site/snippets/partials/address.php
@@ -5,7 +5,8 @@
     86153 Augsburg</a>
   </p>
   <p>
-    <a href="/kontakt">kontakt@das-habitat.de</a>
+    <a href="/kontakt">kontakt@das-habitat.de</a><br />
+    <span title="Wir sind nur bedingt telefonisch erreichbar, rufen euch aber auf jeden Fall zurück.">0821 56732890</span>
   </p>
 
   <h3>Wichtige Informationen</h3>
