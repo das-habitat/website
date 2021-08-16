9755cfe870fce338c21da4ed8a008fe48cc0d3aa
Florian Pichler <fp@ylk.gd>
2021-08-16 22:10:03 +0200

    Remove COVID info

--- kirby/site/snippets/partials/address.php
+++ kirby/site/snippets/partials/address.php
@@ -8,9 +8,4 @@
     <a href="/kontakt">kontakt@das-habitat.de</a><br />
     <span title="Wir sind nur bedingt telefonisch erreichbar, rufen euch aber auf jeden Fall zurück.">0821 56732890</span>
   </p>
-
-  <h3>Wichtige Informationen</h3>
-  <p>
-    Gemäß den von der Stadt Augsburg und der Regierung verkündeten Maßnahmen zu COVID-19 bleibt das Habitat vorerst geschlossen. Für Fragen und Unterstützung nutzt <a href="/kontakt">unser Kontaktformular.</a>
-  </p>
 </div>
